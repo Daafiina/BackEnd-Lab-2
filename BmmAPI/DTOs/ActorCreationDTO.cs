962dbd4707bf96fea847f2e1392b81670009d13f
Jetmir <Jetmirdemiri2002@gmail.com>
2023-11-19 00:22:52 +0100

    Bugs Fixed

--- BmmAPI/DTOs/ActorCreationDTO.cs
+++ BmmAPI/DTOs/ActorCreationDTO.cs
@@ -6,12 +6,12 @@ namespace BmmAPI.DTOs
     {
         [Required]
         [StringLength(120)]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
 
-        public string Biography { get; set; }
+        public string? Biography { get; set; }
 
-        public IFormFile Picture { get; set; }
+        public IFormFile? Picture { get; set; }
     }
 }
--- BmmAPI/DTOs/MovieCreationDTO.cs
+++ BmmAPI/DTOs/MovieCreationDTO.cs
@@ -5,19 +5,19 @@ namespace BmmAPI.DTOs
 {
     public class MovieCreationDTO
     {
-        public string Title { get; set; }
-        public string Summary { get; set; }
-        public string Trailer { get; set; }
-        public bool InTheaters { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public IFormFile Poster { get; set; }
+        public string? Title { get; set; }
+        public string? Summary { get; set; }
+        public string? Trailer { get; set; }
+        public bool? InTheaters { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public IFormFile? Poster { get; set; }
 
 
         [ModelBinder(BinderType = typeof(TypeBinder<List<int>>))]
-        public List<int> GenresIds { get; set; }
+        public List<int>? GenresIds { get; set; }
 
         [ModelBinder(BinderType = typeof(TypeBinder<List<int>>))]
-        public List<int> MovieTheatersIds { get;set; }
+        public List<int>? MovieTheatersIds { get;set; }
 
 
         //[ModelBinder(BinderType = typeof(TypeBinder<List<MoviesActorsCreationDTO>>))]
